558938f7b5d2d372ef6a39bb13a9b810892f4f47
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-10-16 09:50:28 -0500

    Maxima suma de un subarreglo en O(nlg(n)).

--- divide_y_venceras/p04.c
+++ divide_y_venceras/p04.c
@@ -2,18 +2,58 @@
 #include <stdlib.h>
 
 const int INF = 1e9;
-int* acc;
 
-int max(int a, int b) {
-  return ((a > b) ? a : b);
+typedef struct {
+  int ind_izq;
+  int ind_der;
+  int suma;
+} Resultado;
+
+void Asignar(Resultado* resultado, int ind_izq, int ind_der, int suma) {
+  (*resultado).ind_izq = ind_izq;
+  (*resultado).ind_der = ind_der;
+  (*resultado).suma = suma;
 }
 
-int MaxSum(int* a, int l, int r) {
-  if (l > r) return -INF;
-  int sum1 = acc[r] - ((l > 0) ? acc[l - 1] : 0);
-  int sum2 = MaxSum(a, l + 1, r);
-  int sum3 = MaxSum(a, l, r - 1);
-  return max(sum1, max(sum2, sum3));
+Resultado MaximaSumaCruzada(int* a, int l, int m, int r) {
+  int izq = -INF;
+  int ind_izq;
+  int aux = 0;
+  for (int i = m; i >= l; i--) {
+    aux += a[i];
+    if (aux > izq) {
+      izq = aux;
+      ind_izq = i;
+    }
+  }
+  int der = -INF;
+  int ind_der;
+  aux = 0;
+  for (int i = m + 1; i <= r; i++) {
+    aux += a[i];
+    if (aux > der) {
+      der = aux;
+      ind_der = i;
+    }
+  }
+  Resultado resultado;
+  Asignar(&resultado, ind_izq, ind_der, izq + der);
+  return resultado;
+}
+
+Resultado MaximaSuma(int* a, int l, int r) {
+  if (l == r) {
+    Resultado resultado;
+    Asignar(&resultado, l, r, a[l]);
+    return resultado;
+  }
+  int m = (l + r) / 2;
+  Resultado izq = MaximaSuma(a, l, m);
+  Resultado der = MaximaSuma(a, m + 1, r);
+  Resultado cruz = MaximaSumaCruzada(a, l, m, r);
+  if (izq.suma >= der.suma && izq.suma >= cruz.suma) return izq;
+  if (der.suma >= izq.suma && der.suma >= cruz.suma) return der;
+  return cruz;
 }
 
 int main(void) {
@@ -21,9 +61,10 @@ int main(void) {
   scanf("%d", &n);
   int* a = malloc(sizeof(int) * n);
   for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-  acc = malloc(sizeof(int) * n);
-  acc[0] = a[0];
-  for (int i = 1; i < n; i++) acc[i] = acc[i - 1] + a[i];
-  printf("Maxima suma: %d\n", MaxSum(a, 0, n - 1));
+  Resultado respuesta = MaximaSuma(a, 0, n - 1);
+  printf("Maxima Suma: %d\n", respuesta.suma);
+  printf("Subarreglo: a[%d, %d] =", respuesta.ind_izq, respuesta.ind_der);
+  for (int i = respuesta.ind_izq; i <= respuesta.ind_der; i++) printf(" %d", a[i]);
+  printf("\n");
   return 0;
 }
