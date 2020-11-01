868bc2a539ce4cff7756f11f06fb0a72d7b0c6da
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-11-01 13:12:41 -0500

    Mi cola.

---
+++ examen_parcial/20185048_EX1_P01/cola.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "nodo.h"
+#include "cola.h"
+
+void CrearCola(Cola* ref_cola) {
+  *ref_cola = NULL;
+}
+
+int EstaVacia(Cola cola) {
+  return (cola == NULL);
+}
+
+void Encolar(Cola* ref_cola, Dato valor) {
+  Nodo* ultimo = (Nodo*) malloc(sizeof(Nodo));
+  ultimo->elemento = valor;
+  ultimo->siguiente = *ref_cola;
+  *ref_cola = ultimo;
+}
+
+Dato Desencolar(Cola* ref_cola) {
+  //assert(!EstaVacia(*ref_cola));
+  Nodo* anterior = NULL;
+  Nodo* actual = *ref_cola;
+  if (!EstaVacia(actual)) {
+    while (actual->siguiente != NULL) {
+      anterior = actual;
+      actual = actual->siguiente;
+    }
+    if (anterior != NULL) {
+      anterior->siguiente = NULL;
+    } else {
+      *ref_cola = NULL;
+    }
+    Dato valor = actual->elemento;
+    free(actual);
+    return valor;
+  }
+}
+
+void ImprimirCola(Cola cola) {
+  Nodo* actual = cola;
+  while (actual != NULL) {
+    printf("%d ", actual->elemento);
+    actual = actual->siguiente;
+  }
+  printf("\n");
+  //printf("NULL\n");
+}
---
+++ examen_parcial/20185048_EX1_P01/cola.h
@@ -0,0 +1,58 @@
+/*
+ * Archivo: cola.h
+ * ---------------
+ * Esta interfaz exporta los metodos de una cola, una coleccion en la
+ * cual sus valores son procesados en el orden first-in / first-out (FIFO).
+ */
+
+#ifndef _cola_h
+#define _cola_h
+
+typedef Nodo* Cola;
+
+/*
+ * Metodo: CrearCola
+ * Uso: CreaCola(&cola);
+ * ---------------------
+ * Inicializa una cola vacia en NULL.
+ */
+
+void CrearCola(Cola* ref_cola);
+
+/*
+ * Metodo: EstaVacia
+ * Uso: if (EstaVacia(cola)) . . .
+ * -------------------------------
+ * Retorna 1 si la cola no contiene elementos.
+ */
+
+int EstaVacia(Cola cola);
+
+/*
+ * Metodo: Encolar
+ * Uso: Encolar(&cola, valor);
+ * ---------------------------
+ * Anade el valor especificado al final de la cola.
+ */
+
+void Encolar(Cola* ref_cola, Dato valor);
+
+/*
+ * Metodo: Desencolar
+ * Uso: Dato primero = Desencolar(&cola);
+ * --------------------------------------
+ * Elimina y retorna el primer elemento en la cola.
+ */
+
+Dato Desencolar(Cola* ref_cola);
+
+/*
+ * Metodo: ImprimirCola
+ * Uso: ImprimirCola(cola);
+ * ------------------------
+ * Imprime la cola desde el ultimo elemento hasta el primero.
+ */
+
+void ImprimirCola(Cola cola);
+
+#endif
