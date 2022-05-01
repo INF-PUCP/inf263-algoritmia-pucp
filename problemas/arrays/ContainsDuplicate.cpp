e4c068b57558f0b046422c4059e38dd200751538
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2022-05-01 15:26:24 -0500

    sort

---
+++ problemas/arrays/ContainsDuplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  bool containsDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = (int) nums.size();
+    for (int i = 1; i < n; i++) {
+      if (nums[i] == nums[i - 1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
