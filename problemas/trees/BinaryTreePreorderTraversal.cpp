5953d66ba234386a9fa60abb5a99fb1a8dc488e4
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-12 20:05:28 -0500

    preorder

---
+++ problemas/trees/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> traversal;
+        DFS(root, traversal);
+        return traversal;
+    }
+private:
+    void DFS(TreeNode* root, vector<int>& traversal) {
+        if (root == nullptr) return;
+        traversal.push_back(root->val);
+        DFS(root->left, traversal);
+        DFS(root->right, traversal);
+    }
+};
