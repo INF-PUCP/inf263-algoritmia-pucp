d9518301b1e70b0d3518a199c1a100c15469c0ea
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-12 19:49:03 -0500

    inorder

--- problemas/trees/BinaryTreeInorderTraversal.cpp
+++ problemas/trees/BinaryTreeInorderTraversal.cpp
@@ -1,28 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    vector<int> tour;
-    
-    void DFS(TreeNode* root) {
-        if (root == nullptr) return;
-        DFS(root->left);
-        tour.push_back(root->val);
-        DFS(root->right);
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        tour.clear();
-        DFS(root);
-        return tour;
+        vector<int> traversal;
+        DFS(root, traversal);
+        return traversal;
+    }
+private:
+    void DFS(TreeNode* root, vector<int>& traversal) {
+        if (root == nullptr) return;
+        DFS(root->left, traversal);
+        traversal.push_back(root->val);
+        DFS(root->right, traversal);
     }
 };
