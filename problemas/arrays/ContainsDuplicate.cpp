55453cbcd6245a4ea252d6a049827a99e5b3b0af
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-17 11:53:57 -0500

    hash set

--- problemas/arrays/ContainsDuplicate.cpp
+++ problemas/arrays/ContainsDuplicate.cpp
@@ -1,13 +1,14 @@
+// https://leetcode.com/problems/contains-duplicate/
 class Solution {
 public:
-  bool containsDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int n = (int) nums.size();
-    for (int i = 1; i < n; i++) {
-      if (nums[i] == nums[i - 1]) {
-        return true;
-      }
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seenNumbers;
+        for (const int number : nums) {
+            if (seenNumbers.count(number) > 0) {
+                return true;
+            }
+            seenNumbers.insert(number);
+        }
+        return false;
     }
-    return false;
-  }
 };
