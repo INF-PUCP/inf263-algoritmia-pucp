2bfbda913a9067c2e9a5ebb971deee02efdf58ab
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-20 10:32:25 -0500

    trivial

--- problemas/arrays/ValidAnagram.cpp
+++ problemas/arrays/ValidAnagram.cpp
@@ -1,11 +1,19 @@
+// https://leetcode.com/problems/valid-anagram/
 class Solution {
+private:
+    const int ALPHABET_SIZE = 26;
+    const int a = int('a');
+
+    vector<int> getFrequencies(const string& s) {
+        vector<int> frequencies(ALPHABET_SIZE, 0);
+        for (char c : s) {
+            frequencies[int(c) - a]++;
+        }
+        return frequencies;
+    }
+
 public:
-  vector<int> GetFrequency(const string& s) {
-    vector<int> cnt(26, 0);
-    for (char c : s) cnt[c - 'a']++;
-    return cnt;
-  }
-  bool isAnagram(string s, string t) {
-    return GetFrequency(s) == GetFrequency(t);
-  }
+    bool isAnagram(string s, string t) {
+        return getFrequencies(s) == getFrequencies(t);
+    }
 };
