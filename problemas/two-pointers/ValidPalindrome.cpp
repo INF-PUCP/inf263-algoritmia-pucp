2bf05398461ae0a4f438ddce258f7cd0680240d6
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-22 00:46:07 -0500

    palindrome

--- problemas/two-pointers/ValidPalindrome.cpp
+++ problemas/two-pointers/ValidPalindrome.cpp
@@ -1,51 +1,19 @@
-// https://leetcode.com/problems/valid-palindrome/submissions/
-
+// https://leetcode.com/problems/valid-palindrome/
 class Solution {
- public:
-  bool IsDigit(char c) {
-    return ('0' <= c && c <= '9');
-  }
-
-  bool IsLower(char c) {
-    return ('a' <= c && c <= 'z');
-  }
-
-  bool IsUpper(char c) {
-    return ('A' <= c && c <= 'Z');
-  }
-
-  bool IsAlphanumeric(char c) {
-    return IsDigit(c) || IsLower(c) || IsUpper(c);
-  }
-
-  char FormatChar(char c) {
-    if (IsDigit(c)) return c;
-    if (IsLower(c)) return c;
-    return tolower(c);
-  }
-
-  string GetAlphanumeric(string s) {
-    string alphanumeric = "";
-    for (char c : s) {
-      if (IsAlphanumeric(c)) {
-        alphanumeric += FormatChar(c);
-      }
+public:
+    bool isPalindrome(string s) {
+        string cleanString;
+        for (char c : s) {
+            if (isalnum(c)) {
+                cleanString += tolower(c);
+            }
+        }
+        int n = int(cleanString.size());
+        for (int i = 0; i < n / 2; i++) {
+            if (cleanString[i] != cleanString[n - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
     }
-    return alphanumeric;
-  }
-
-  bool IsPalindrome(string s) {
-    int n = (int)s.size();
-    for (int i = 0; i < n / 2; i++) {
-      if (s[i] != s[n - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  bool isPalindrome(string s) {
-    string alphanumeric = GetAlphanumeric(s);
-    return IsPalindrome(alphanumeric);
-  }
 };
