37d43e290698fc52a92efc21b0ef37139f324a1a
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-24 14:29:16 -0500

    floyd

---
+++ problemas/lists/LinkedListCycle.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/linked-list-cycle/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (slow != fast) {
+            if (fast == nullptr || fast->next == nullptr) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return true;
+    }
+};
