0599e931f9f39d8f1f21d3235dafc2f9913296d1
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2024-05-31 14:01:55 -0500

    recursion

--- problemas/trees/ValidateBinarySearchTree.cpp
+++ problemas/trees/ValidateBinarySearchTree.cpp
@@ -1,43 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-typedef long long Long;
-const Long INF = LLONG_MAX;
+// https://leetcode.com/problems/validate-binary-search-tree/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
-public:
-    
-    Long Max(TreeNode* root) {
-        Long ans = -INF;
-        if (root == nullptr) return ans;
-        ans = max(ans, (Long) root->val);
-        ans = max(ans, Max(root->left));
-        ans = max(ans, Max(root->right));
-        return ans;
-    }
-    
-    Long Min(TreeNode* root) {
-        Long ans = INF;
-        if (root == nullptr) return ans;
-        ans = min(ans, (Long) root->val);
-        ans = min(ans, Min(root->left));
-        ans = min(ans, Min(root->right));
-        return ans;
-    }
-    
-    bool isValidBST(TreeNode* root) {
-        if (root == nullptr) return true;
-        if (isValidBST(root->left) && isValidBST(root->right) &&
-            Max(root->left) < root->val && Min(root->right) > root->val)
+private:
+    bool isValidBST(TreeNode* root, long long inf, long long sup) {
+        if (root == nullptr) {
             return true;
-        return false;
+        }
+
+        if (inf >= root->val || root->val >= sup) {
+            return false;
+        }
+
+        return isValidBST(root->left, inf, root->val) &&
+        isValidBST(root->right, root->val, sup);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, LLONG_MIN, LLONG_MAX);
     }
 };
