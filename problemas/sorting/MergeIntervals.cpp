d2b39fa0544a9b7e853f9183d7f4214a476ee392
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2025-09-13 16:14:36 -0500

    time: O(nlgn), space: O(n)

--- problemas/sorting/MergeIntervals.cpp
+++ problemas/sorting/MergeIntervals.cpp
@@ -1,33 +1,20 @@
 // https://leetcode.com/problems/merge-intervals/
-
 class Solution {
 public:
-    typedef vector<int> Interval;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::vector<std::vector<int>> merged_intervals;
+        std::sort(intervals.begin(), intervals.end());
 
-    bool Overlap(const Interval &a, const Interval &b) {
-        return a[1] >= b[0] && a[0] <= b[1];
-    }
-
-    Interval Merge(const Interval &a, const Interval &b) {
-        Interval merged_interval = {min(a[0], b[0]), max(a[1], b[1])};
-        return merged_interval;
-    }
-
-    vector<vector<int>> merge(vector<vector<int>> &intervals) {
-        sort(intervals.begin(), intervals.end());
-        int n_intervals = (int)intervals.size();
-        vector<Interval> merged_intervals;
-        Interval merged_interval = intervals[0];
-        for (int i = 1; i < n_intervals; i++) {
-            if (Overlap(merged_interval, intervals[i])) {
-                merged_interval = Merge(merged_interval, intervals[i]);
-            }
-            else {
-                merged_intervals.push_back(merged_interval);
-                merged_interval = intervals[i];
+        std::vector<int> interval{intervals[0]};
+        for (size_t i = 1; i < intervals.size(); i++) {
+            if (interval[0] <= intervals[i][0] && intervals[i][0] <= interval[1]) {
+                interval[1] = std::max(interval[1], intervals[i][1]);
+            } else {
+                merged_intervals.push_back(interval);
+                interval = intervals[i];
             }
         }
-        merged_intervals.push_back(merged_interval);
+        merged_intervals.push_back(interval);
         return merged_intervals;
     }
 };
